c92de97919cfcfc99618022e79e2744a90aba324
Zack Xu <zackxu419@outlook.com>
2023-03-13 08:56:50 -0400

    a

--- detect.py
+++ detect.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import pprint
 # import picamera
 
 # # Get Image
@@ -9,13 +10,94 @@ import numpy as np
 #     camera.capture(output, 'rgb')
 #     camera.close()
 
-output = cv.imread('2.jpg')
+# Returns bottom left of the board and top right of the board
+
+
+def edge_det(output):
+    final_res = []
+    # with picamera.PiCamera() as camera:
+    #     camera.resolution = (640, 480)
+    #     rawCapture = PiRGBArray(camera)
+    #     camera.capture(rawCapture, format="bgr")
+    #     output = rawCapture.array
+    #     camera.close()
+
+    hsv_img = cv.cvtColor(output, cv.COLOR_RGB2HSV)
+
+    # lower boundary RED color range values; Hue (0 - 10)
+    lower1 = np.array([0, 100, 100])
+    upper1 = np.array([10, 255, 255])
+
+    # upper boundary RED color range values; Hue (160 - 180)
+    lower2 = np.array([100, 100, 100])
+    upper2 = np.array([120, 255, 255])
+
+    lower_mask = cv.inRange(hsv_img, lower1, upper1)
+    upper_mask = cv.inRange(hsv_img, lower2, upper2)
+
+    full_mask = lower_mask + upper_mask
+
+    # Apply the mask to the original image
+    result = cv.bitwise_and(output, output, mask=full_mask)
+    cv.imshow("r", result)
+
+    gray = cv.cvtColor(cv.cvtColor(
+        result, cv.COLOR_HSV2RGB), cv.COLOR_RGB2GRAY)
+
+    contours, _ = cv.findContours(gray, 1, 2)
+
+    avg = np.zeros((len(contours), 2))
+
+    for i in range(len(contours)):
+        a = np.array([0, 0])
+        counter = 0
+        for j in contours[i]:
+            counter += 1
+            a[0] += j[0][0]
+            a[1] += j[0][1]
+        a = a/counter
+        avg[i] = (a)
+
+    # print(list(avg))
+
+    for i in list(avg):
+        print(i)
+        # If is toward the center, throw it away
+        centerLeftT = 640/2-50
+        centerRightT = 640/2+50
+        if i[0] < centerLeftT or i[0] > centerRightT:
+            final_res.append(i)
+    # if len(final_res) != 4:
+    #     return "ERROR"
+
+    # Find bottom left and top right (The position that have: (lowY, highX), (highY, lowX))
+    ffinal_res = [0, 0]
+    for i in final_res:
+        if(i[0] > 400 and i[1] < 200):
+            ffinal_res[1] = i
+        if(i[1] > 400 and i[0] < 200):
+            ffinal_res[0] = i
+
+    return ffinal_res
+
+
+output = cv.imread('6.jpg')
+
+# Crop image:
+# Detect the red dots
+detectedEdges = edge_det(output=output)
+print(detectedEdges)
+# output = output[int(detectedEdges[0][0]):int(detectedEdges[1]
+#                 [0]), int(detectedEdges[1][1]):int(detectedEdges[0][1])]
+output = output[int(detectedEdges[1][1]):int(detectedEdges[0][1]), int(
+    detectedEdges[0][0]):int(detectedEdges[1][0])]
+cv.imshow("cropped", output)
 
 hsv_img = cv.cvtColor(output, cv.COLOR_BGR2HSV)
 
 # Define the range of green color in HSV
-lower_green = np.array([60, 100, 100])
-upper_green = np.array([180, 255, 255])
+lower_green = np.array([50, 58, 58])
+upper_green = np.array([200, 255, 255])
 
 # Create a mask for green color
 green_mask = cv.inRange(hsv_img, lower_green, upper_green)
@@ -27,20 +109,56 @@ edges = cv.Canny(cv.cvtColor(cv.cvtColor(
     result, cv.COLOR_HSV2BGR), cv.COLOR_BGR2GRAY), 50, 100)
 cv.imshow("edge", edges)
 
+
 circles = cv.HoughCircles(edges, cv.HOUGH_GRADIENT, 1,
-                          minRadius=10, maxRadius=30, param2=20, minDist=50)
-print(len(circles))
+                          minRadius=5, maxRadius=13, param2=15, minDist=25)
+
+
+# print(len(circles))
 # ensure at least some circles were found
 if circles is not None:
     # convert the (x, y) coordinates and radius of the circles to integers
     circles = np.round(circles[0, :]).astype("int")
     # loop over the (x, y) coordinates and radius of the circles
     for (x, y, r) in circles:
-        print(x, y, r)
+        # print(x, y, r)
         # draw the circle in the output image, then draw a rectangle
         # corresponding to the center of the circle
         cv.circle(output, (x, y), r, (0, 255, 0), 4)
         cv.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+
+# Drawing the pixles to divide up the grid, development purposes only
+dify = int(len(output)/8)
+difx = int(len(output[0])/8)
+const = 10
+for i in range(8):
+    for j in range(8):
+        cv.rectangle(output, (const+i * difx, j * dify),
+                     (const+(i+1) * difx, (j+1) * dify), (0, 0, 255), 1)
+
+# Start from the top.
+
+board = []
+for j in range(8):
+    # For each row
+    row = []
+    for i in range(8):
+        rect = [const+i * difx, j * dify, const+(i+1) * difx,
+                (j+1) * dify]  # xMin, yMin, xMax, yMax
+        print(rect)
+        # check if a center of a circle is in that range
+        isSomething = False
+        for (x, y, r) in circles:
+            if x > rect[0] and x < rect[2] and y > rect[1] and y < rect[3]:
+                isSomething = True
+                row.append(0)
+        if not isSomething:
+            row.append(1)
+    board.append(row)
+
+pprint.pprint(board)
+
 
 cv.imshow("Res", result)
 cv.imshow("In", output)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from time import sleep
 import picamera
 from picamera.array import PiRGBArray
+import pprint
 
 # Constants
 IP = '0.0.0.0'
@@ -42,24 +43,24 @@ def move(x=0, y=0, z=10, home=False, speed=3000, boardForward=False):
         f'http://{IP}/api/printer/command', headers=headers, json=json_data))
 
 
-def edge_det():
+def edge_det(output):
     final_res = []
-    with picamera.PiCamera() as camera:
-        camera.resolution = (640, 480)
-        rawCapture = PiRGBArray(camera)
-        camera.capture(rawCapture, format="bgr")
-        output = rawCapture.array
-        camera.close()
+    # with picamera.PiCamera() as camera:
+    #     camera.resolution = (640, 480)
+    #     rawCapture = PiRGBArray(camera)
+    #     camera.capture(rawCapture, format="bgr")
+    #     output = rawCapture.array
+    #     camera.close()
 
     hsv_img = cv.cvtColor(output, cv.COLOR_RGB2HSV)
 
     # lower boundary RED color range values; Hue (0 - 10)
-    lower1 = np.array([0, 100, 20])
+    lower1 = np.array([0, 100, 100])
     upper1 = np.array([10, 255, 255])
 
     # upper boundary RED color range values; Hue (160 - 180)
-    lower2 = np.array([160, 100, 20])
-    upper2 = np.array([179, 255, 255])
+    lower2 = np.array([100, 100, 100])
+    upper2 = np.array([120, 255, 255])
 
     lower_mask = cv.inRange(hsv_img, lower1, upper1)
     upper_mask = cv.inRange(hsv_img, lower2, upper2)
@@ -95,8 +96,87 @@ def edge_det():
         centerRightT = 640/2+50
         if i[0] < centerLeftT or i[0] > centerRightT:
             final_res.append(i)
-    return final_res
+    # if len(final_res) != 4:
+    #     return "ERROR"
 
+    # Find bottom left and top right (The position that have: (lowY, highX), (highY, lowX))
+    ffinal_res = [0, 0]
+    for i in final_res:
+        if(i[0] > 400 and i[1] < 200):
+            ffinal_res[1] = i
+        if(i[1] > 400 and i[0] < 200):
+            ffinal_res[0] = i
+
+    return ffinal_res
+
+
+def getBoardState(output):
+    detectedEdges = edge_det(output=output)
+    output = output[int(detectedEdges[1][1]):int(detectedEdges[0][1]), int(
+        detectedEdges[0][0]):int(detectedEdges[1][0])]
+
+    hsv_img = cv.cvtColor(output, cv.COLOR_BGR2HSV)
+
+    # Define the range of green color in HSV
+    lower_green = np.array([50, 58, 58])
+    upper_green = np.array([200, 255, 255])
+
+    # Create a mask for green color
+    green_mask = cv.inRange(hsv_img, lower_green, upper_green)
+
+    # Apply the mask to the original image
+    result = cv.bitwise_and(output, output, mask=green_mask)
+
+    edges = cv.Canny(cv.cvtColor(cv.cvtColor(
+        result, cv.COLOR_HSV2BGR), cv.COLOR_BGR2GRAY), 50, 100)
+
+    circles = cv.HoughCircles(edges, cv.HOUGH_GRADIENT, 1,
+                              minRadius=5, maxRadius=13, param2=15, minDist=25)
+
+    # print(len(circles))
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # print(x, y, r)
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv.rectangle(output, (x - 5, y - 5),
+                         (x + 5, y + 5), (0, 128, 255), -1)
+
+    # Drawing the pixles to divide up the grid, development purposes only
+    dify = int(len(output)/8)
+    difx = int(len(output[0])/8)
+    const = 10
+    for i in range(8):
+        for j in range(8):
+            cv.rectangle(output, (const+i * difx, j * dify),
+                         (const+(i+1) * difx, (j+1) * dify), (0, 0, 255), 1)
+
+    # Start from the top.
+
+    board = []
+    for j in range(8):
+        # For each row
+        row = []
+        for i in range(8):
+            rect = [const+i * difx, j * dify, const+(i+1) * difx,
+                    (j+1) * dify]  # xMin, yMin, xMax, yMax
+            print(rect)
+            # check if a center of a circle is in that range
+            isSomething = False
+            for (x, y, r) in circles:
+                if x > rect[0] and x < rect[2] and y > rect[1] and y < rect[3]:
+                    isSomething = True
+                    row.append(0)
+            if not isSomething:
+                row.append(1)
+        board.append(row)
+
+    return board
 
 # Calibrate - Remove ALL CHESS PIECES before calling this
 # def calibrate():
@@ -118,6 +198,17 @@ def edge_det():
 
 # Main Program
 # calibrate()
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    rawCapture = PiRGBArray(camera)
+    camera.capture(rawCapture, format="bgr")
+    output = rawCapture.array
+    camera.close()
+
+
 move(home=True)
 move()
 move(boardForward=True)
+sleep(10)
+pprint.pprint(getBoardState)
