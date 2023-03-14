efd4294473277a82d7ad2d4eaf8cbab9a5f9f5a8
Zack Xu <zackxu419@outlook.com>
2023-03-14 15:04:07 -0400

    a

--- main.py
+++ main.py
@@ -250,7 +250,7 @@ def rundet():
         fromWhite.append(i[::-1])
     if previousRes != []:
         pieceFrom = getBoardDiff(fromWhite)
-        print(pieceFrom + "FROM")
+        print(pieceFrom.__str__() + "FROM")
         try:
             pieceFrom = f"{letters[pieceFrom[1]]}{8-pieceFrom[0]}"
         except:
