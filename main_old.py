50ef4c0207f1d46ed0000a5fb03ee697fbcf1c7f
Zack Xu <zackxu419@outlook.com>
2023-03-16 14:31:32 -0400

    a

--- main_old.py
+++ main_old.py
@@ -222,7 +222,7 @@ def getBoardState(output, edges=[0, 0, 0, 0]):
         board.append(row)
 
     sortedDiff = sorted(bigDiff, reverse=True)
-    final_res = sortedDiff[0:2].values()
+    final_res = sortedDiff[0:2]
 
     # After comparing, set the previmage to curr image
     previousImg = gray_orig
