fb749da9662c56d2d9a4f2a057f2b189ede76647
Zack Xu <zackxu419@outlook.com>
2023-03-20 15:38:54 -0400

    a

--- main_v2.py
+++ main_v2.py
@@ -335,9 +335,9 @@ def getMove():
             letter2 = i
 
     print(letter1, number1)
-    previousRes[8-int(number1)][letter1] == 0
+    previousRes[8-int(number1)][letter1] = 0
     print(letter2, number2)
-    previousRes[8-int(number2)][letter2] == 2
+    previousRes[8-int(number2)][letter2] = 2
     pprint.pprint(previousRes)
 
     return best_move
