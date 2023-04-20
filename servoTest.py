f01307f12f639012e24a83fbdcaf9cf53bfb4c8d
Zack Xu <zackxu419@outlook.com>
2023-04-20 14:37:21 -0400

    a

---
+++ servoTest.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+servo_pin = 13
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+pwm = GPIO.PWM(servo_pin, 50)  # Set the PWM frequency to 50 Hz
+pwm.start(0)
+
+
+def set_angle(angle):
+    duty_cycle = angle / 18 + 2  # Convert angle to duty cycle
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+
+
+set_angle(0)   # Set the servo to the initial position (0 degrees)
+time.sleep(2)  # Wait for 2 seconds
+set_angle(90)  # Set the servo to 90 degrees
+time.sleep(2)  # Wait for 2 seconds
+set_angle(180)  # Set the servo to 180 degrees
+
+pwm.stop()
+GPIO.cleanup()
