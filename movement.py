2e41d027f2bbda154132e6b2217ce3f494730599
Zack Xu <zackxu419@outlook.com>
2023-04-20 14:04:08 -0400

    a

--- movement.py
+++ movement.py
@@ -6,11 +6,16 @@ headers = {
     'X-Api-Key': '041AA6FA66184165A38B8D938C68A30E',
 }
 
+# Add a dictionary to store the last position
+last_position = {
+    'x': 0,
+    'y': 0,
+    'z': 10,
+}
 
-def move(x=0, y=0, z=10, calibrate=False, home=False, speed=3000):
-    json_data = {
-        'command': f'G0 X{x} Y{y} Z{z} F1{speed}'
-    }
+
+def move(x=None, y=None, z=None, calibrate=False, home=False, speed=3000):
+    global last_position
     if calibrate:
         json_data = {
             'command': f'G28'
@@ -19,6 +24,21 @@ def move(x=0, y=0, z=10, calibrate=False, home=False, speed=3000):
         json_data = {
             'command': f'G0 X{0} Y{240} Z{50} F1{3000}'
         }
+    else:
+        # Use the last sent position for unspecified axes
+        x = last_position['x'] if x is None else x
+        y = last_position['y'] if y is None else y
+        z = last_position['z'] if z is None else z
+
+        json_data = {
+            'command': f'G0 X{x} Y{y} Z{z} F1{speed}'
+        }
+
+        # Update the last sent position
+        last_position['x'] = x
+        last_position['y'] = y
+        last_position['z'] = z
+
     print(requests.post(
         'http://0.0.0.0/api/printer/command', headers=headers, json=json_data))
 
