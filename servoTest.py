1bba8558d872fab9f38027d30d5dfff6b3f26df6
Zack Xu <zackxu419@outlook.com>
2023-04-20 14:55:53 -0400

    a

--- servoTest.py
+++ servoTest.py
@@ -10,18 +10,4 @@ pwm.start(0)
 
 
 def set_servo_angle(angle):
-    duty_cycle = (angle / 18.0) + 2.5
-    pwm.ChangeDutyCycle(duty_cycle)
-    time.sleep(0.5)
-
-
-try:
-    while True:
-        set_servo_angle(0)  # Move servo to 0 degrees
-        time.sleep(5)
-        set_servo_angle(90)  # Move servo to 90 degrees
-        time.sleep(5)
-        set_servo_angle(180)  # Move servo to 180 degrees
-finally:
-    pwm.stop()
-    GPIO.cleanup()
+    pwm.ChangeDutyCycle(angle)
