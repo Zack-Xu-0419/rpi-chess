6685b4101293030c1e355bf41af036bb5d8f4166
Zack Xu <zackxu419@outlook.com>
2023-04-20 15:00:21 -0400

    a

--- servoTest.py
+++ servoTest.py
@@ -1,13 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
-servo_pin = 13
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
+# Constants
+SERVO_PIN = 13  # GPIO18 (Pin 12)
+FREQUENCY = 50  # 50Hz frequency (20ms period)
+MIN_DUTY_CYCLE = 2.5
+MAX_DUTY_CYCLE = 12.5
 
-pwm = GPIO.PWM(servo_pin, 50)  # Set PWM frequency to 50Hz
-pwm.start(1000)
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SERVO_PIN, GPIO.OUT)
+    pwm = GPIO.PWM(SERVO_PIN, FREQUENCY)
+    pwm.start(0)
+    return pwm
 
 
-def set_servo_angle(angle):
-    pwm.ChangeDutyCycle(angle)
+def set_angle(pwm, angle):
+    duty_cycle = MIN_DUTY_CYCLE + \
+        (angle / 180.0) * (MAX_DUTY_CYCLE - MIN_DUTY_CYCLE)
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+
+
+def cleanup(pwm):
+    pwm.stop()
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    pwm = setup()
+    try:
+        while True:
+            angle = float(input("Enter angle (0-180): "))
+            set_angle(pwm, angle)
+    except KeyboardInterrupt:
+        cleanup(pwm)
