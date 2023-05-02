1f11b5c3ae666aa908b686ac895c6abde37f783e
Zack Xu <zackxu419@outlook.com>
2023-05-02 14:49:28 -0400

    a

--- final.py
+++ final.py
@@ -257,7 +257,7 @@ def getBoardState(output, edges=[0, 0, 0, 0]):
                                  difx + difx/2-10):int(const+(i+1) * difx-difx/2+10)]
                 avg = np.mean(curr)
                 print(avg)
-                if avg < 120:
+                if avg < 150:
                     row.append(2)
                 else:
                     row.append(1)
