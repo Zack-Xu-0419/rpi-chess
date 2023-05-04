ba523735f34220030a33e45e1b8ae48b741de3a9
Zack Xu <zackxu419@outlook.com>
2023-05-04 14:30:31 -0400

    a

--- final.py
+++ final.py
@@ -564,9 +564,9 @@ BUTTON_PIN = 2
 GPIO.setup(BUTTON_PIN, GPIO.IN)
 
 
-def button_callback():
-    print("Button pressed")
-    det_think_move()
+# def button_callback():
+#     print("Button pressed")
+#     det_think_move()
 
 
 GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING,
@@ -578,6 +578,7 @@ def button_monitor():
         if GPIO.input(BUTTON_PIN) == 0:  # Change to '1' if using a pull-down resistor
             print("Button pressed!")
             # Add a debounce delay to avoid multiple detections
+            det_think_move
             time.sleep(0.3)
 
 
