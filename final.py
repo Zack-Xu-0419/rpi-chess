513f3e8ca43f7b2c15e9fac6a8077f3cfa37e971
Zack Xu <zackxu419@outlook.com>
2023-05-04 14:33:40 -0400

    a

--- final.py
+++ final.py
@@ -578,7 +578,7 @@ def button_monitor():
         if GPIO.input(BUTTON_PIN) == 0:  # Change to '1' if using a pull-down resistor
             print("Button pressed!")
             # Add a debounce delay to avoid multiple detections
-            det_think_move
+            det_think_move()
             time.sleep(0.3)
 
 
