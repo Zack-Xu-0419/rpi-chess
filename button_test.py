1d5113e1d24a06be420041e41bafcb3cf9c30497
Zack Xu <zackxu419@outlook.com>
2023-05-04 14:18:29 -0400

    a

---
+++ button_test.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+BUTTON_PIN = 2
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def button_callback(channel):
+    print("Button pressed!")
+
+
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING,
+                      callback=button_callback, bouncetime=300)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
