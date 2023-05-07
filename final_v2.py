cd0d95c2bca79c84a7eb568e0f4e7af58550a432
Zack Xu <zackxu419@outlook.com>
2023-05-06 23:13:50 -0400

    a

--- final_v2.py
+++ final_v2.py
@@ -424,12 +424,12 @@ def cleanup(pwm):
 
 def close():
     global pwm
-    set_angle(pwm, 70)
+    set_angle(pwm, 60)
 
 
 def open():
     global pwm
-    set_angle(pwm, 50)
+    set_angle(pwm, 40)
 
 
 TOP_Z = 50
