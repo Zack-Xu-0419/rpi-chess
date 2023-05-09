65391d3a38e0fa5b0fb9fde4682eb34ea83bd4df
Zack Xu <zackxu419@outlook.com>
2023-05-09 13:59:41 -0400

    a

--- final.py
+++ final.py
@@ -629,7 +629,7 @@ if __name__ == "__main__":
     pwm = setup()
     setSpeed()
     GPIO.add_event_detect(2, GPIO.FALLING, callback=button_callback)
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port='3000')
 
 # move(calibrate=True)
 # sleep(10)
